5552e0ebdb3387f3aedc447c04e9ccb3a496db5e
NBurns90 <natburns@gmail.com>
2018-02-23 13:58:01 +0000

    search user records by user complete

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -5,7 +5,7 @@ class RecordsController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @records = CheckIn.find(params[:id])
+    @records = CheckIn.where("user_id = ?",params[:id])
 
   end
 
